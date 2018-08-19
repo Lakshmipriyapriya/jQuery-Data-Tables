539e4bead3d6d83663f308dd6899313d12be150c
Lakshmipriyapriya <39649881+Lakshmipriyapriya@users.noreply.github.com>
2018-08-19 13:36:32 +0630

    Add files via upload

--- src/resources/tasks_operations.js
+++ src/resources/tasks_operations.js
@@ -1,11 +1,14 @@
 var Task = require('../../models/task');
-var logger=require('winston')
+var logger=require('winston');
+var _ = require('underscore');
+var idForOperation ;
 var createMyTaskDetails= function (req, res) {
 	var tasks = new Task({
 			title : req.body.title ,
 			 description : req.body.description,
 			 status : req.body.status
 		});
+		
 	return tasks.save(function (err) {
 		if (!err) {
 			logger.info("Task details created");
@@ -29,6 +32,37 @@ var createMyTaskDetails= function (req, res) {
 			logger.error('Internal error(%d): %s', res.statusCode, err.message);
 		}
 	});
-} 
+}
+var getMyTaskDetails = function (req, res) {
+	logger.info("GET all tasks");
+	return Task.find(function (err, task) {
+		if (!err) {
+			 return res.send(task);
+			
+		} else {
+			res.statusCode = 500;
+			logger.error('Internal error(%d): %s', res.statusCode, err.message);
+			 res.json({
+				error : 'Server error'
+			});
+		}
+	});
+}
+var getMyTaskDetailsInArray = function (req,res){
+	return Task.find(function(error,tasks){
+		idForOperation = tasks._id;
+		var getDataFromDb=tasks.map (function getAllDetails(tasks,index){
+			var taskData=[tasks.title,tasks.description];
+			return taskData;
+		});
+		
+		var getDataFromDataTable = {
+			data:[getDataFromDb]
+		}
+      console.log("TaskList created");
+      res.send(getDataFromDb);	
+	})	
+ }
+ exports.getMyTaskDetailsInArray=getMyTaskDetailsInArray
+ exports.getMyTaskDetails=getMyTaskDetails
  exports.createMyTaskDetails=createMyTaskDetails
-
